31be9e143d0ec2bed54376e73897d5c0bc067d9f
gustavodias <gustavodias@itera.com.br>
2021-06-29 23:59:35 +0300

    7:07

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+// this is the app entry point
 namespace Catalog
 {
     public class Program
--- Startup.cs
+++ Startup.cs
@@ -21,10 +21,10 @@ namespace Catalog
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; } /* Config properties: use any time you need to read some config info from multiple sources like env variables or files, config that you don't want to hard code in your services*/
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) /* Service Registration: register all the services that you are going to be using across the app*/
         {
 
             services.AddControllers();
@@ -35,7 +35,7 @@ namespace Catalog
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) /*Request handling pipeline config:  this is where you config a request pipeline on ASP.NET, you defines tipical midlewares (additional components that will excute before your controller or your code actually executes) | each executes as your request comes into the ASP.NET process from there until your code executes*/
         {
             if (env.IsDevelopment())
             {
